bb63851f158695d817ec6b6f3a1c2bf64685f279
David Beck <david@rotundasoftware.com>
2013-05-14 16:08:16 -0700

    changed _.size call to use _.uniqueId

--- backbone-nested.js
+++ backbone-nested.js
@@ -9,7 +9,7 @@
       if (!(view instanceof View)) {
         throw new Error("Subview must be a Backbone.View");  
       }
-      if (_.isUndefined(key)) key = _.size(this.subviews);
+      if (_.isUndefined(key)) key = _.uniqueId('subview');
       (this.subviews || (this.subviews = {}))[key] = (view);
       return view;
     },
